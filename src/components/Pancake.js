23f00ea67482e614e349cfc50fcb17a2bf97b480
internap627 <you@example.com>
2018-11-22 15:53:32 +0000

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -16,7 +16,7 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentWillUnmount() which will clear the interval
-  componentWillMount(){
+  componentWillUnmount(){
     this.cleanUpInterval()
   }
 
